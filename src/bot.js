ac95d454cf1bc3df45a9bcfd5d3025be4ff4a2dc
Tóth Norbert <tnorbert201@gmail.com>
2023-08-20 21:54:53 +0200

    committed 0.0.0.3

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 const { token } = process.env;
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const fs = require('fs');
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
 
-const client = Client({ intents: GatewayIntentBits.Guilds});
+const client = Client({ intents: GatewayIntentBits.Guilds });
 client.commands = new Collection();
+
+const functionFolder = fs.readSync(`./src/functions`);
+for (const folder of functionFolder) {
+  const functionFiles = fs
+    .readdirSync(`./src/functions/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of functionFiles)
+    require(`./functions/${folder}/${file}`)(client);
+}
