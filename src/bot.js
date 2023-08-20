f338b75acec6d306c1f64144e5b0b578731e7c97
Tóth Norbert <tnorbert201@gmail.com>
2023-08-20 15:56:16 +0200

    commit 0.0.0.2

---
+++ src/bot.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const { token } = process.env;
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+
+const client = Client({ intents: GatewayIntentBits.Guilds});
+client.commands = new Collection();
