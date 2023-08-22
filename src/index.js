0d0abb88dca42543a5753e32c89659cdac12c65b
Tóth Norbert <tnorbert201@gmail.com>
2023-08-22 13:23:26 +0200

    Test version 0.0.0.1 Első teszt commit: 0.0.0.7

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-const { Client, IntentsBitField} = require("discord.js");
+require('dotenv').config();
+const { Client, IntentsBitField, MessageReaction } = require('discord.js');
 
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
-    ],
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
 
+client.on('ready', (client) => {
+  console.log(`✅ ${client.user.tag} is login and online ✅`);
+})
+
+client.on('messageCreate', (message) => {
+  if(message.content == 'Alex better than Cool Doggo'){
+    message.reply('Is it  not true❗');
+  }
+})
+
+
+client.login(process.env.TOKEN);
